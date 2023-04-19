acaecd2aaa1857918f71100570506051c7a45e7d
steph-edu <stephanyeduarda302@gmail.com>
2023-04-19 17:47:07 -0300

    menu sobre

--- src/public/index.php
+++ src/public/index.php
@@ -9,7 +9,35 @@
                         echo $time[2];
                         echo "<br />";
                         print_r($time);
+                        echo "a: [" . TRUE . "]<br /> ";
+                        echo "a: [" . FALSE . "]<br /> ";
+                        echo "a: [" . (20 > 9) . "]<br /> ";
+                        echo "b: [" . (5 == 6) . "]<br /> ";
+                        echo "c: [" . (1 == 0) . "]<br /> ";
+                        echo "d: [" . (1 == 1) . "]<br /> ";
+
                         
+                        $nome = "Maria";
+                        echo 'f: [' . $nome . '] <br />';
+
+                        $menu = 'sobre';
+                        switch ($menu) {
+                                case 'index':
+                                        echo 'Escolha a página Inicial.';
+                                        break;
+                                case 'sobre':
+                                        echo 'Escolheu a página Sobre.';
+                                        break;
+                                case 'novidades':
+                                        echo 'Escolheu a página Novidades.';
+                                        break;
+                                case 'link':
+                                        echo 'Escolheu a página Links.';
+                                        break;
+                                default:
+                                        echo 'Página não encontrada.';
+                                        break;
+                        };
 
                                 /*
                         
