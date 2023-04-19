460ad3b43b6863bbde20909d50a5191f5664c658
steph-edu <stephanyeduarda302@gmail.com>
2023-04-19 16:55:07 -0300

    codigo da maria jose ana

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,24 @@
-    <?php
-            $primeiro_nome = "Maria";
-            echo $primeiro_nome;
-            echo '<br />';
-            $ultimo_nome = "Lourdes";
-            echo $ultimo_nome;
-    ?>
+<html>
+        <head>
+                <title>PHP TEST</title>
+        </head>
+        <body>
+    
+                <?php
+                        $time = array("Maria", "José", "Ana");
+                        echo $time[2]
+                        echo "<br />";
+                        print_r($time);
+                        
+
+                                /*
+                        
+                        $primeiro_nome = "Maria";
+                        echo $primeiro_nome;
+                        echo '<br />';
+                        $ultimo_nome = "Lourdes";
+                        echo $ultimo_nome; """
+                        */
+                ?>
+        </body>
+</html>
