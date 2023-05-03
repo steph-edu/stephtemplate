dbb43c6410a39d25189d64e3a7327dde43a6bd93
steph-edu <stephanyeduarda302@gmail.com>
2023-05-03 17:39:42 -0300

    adicioado pasta

---
+++ src/public/facebook-clone/posts.php
@@ -0,0 +1,69 @@
+<?php
+$posts = array(
+  array(
+    'name' => 'Lili',
+    'title' => 'Festa no parque',
+    'description' => 'Não perca a grande festa no parque amanhã! Teremos música ao vivo, comida e muita diversão para toda a família.',
+    'image' => 'http://example.com/images/park-party.jpg',
+    'link' => 'http://example.com/park-party',
+    'date' => '2023-05-15'
+  ),
+  array(
+    'name' => 'Lili',
+    'title' => 'Promoção da semana',
+    'description' => 'Aproveite nossa promoção da semana! Todos os produtos com 20% de desconto.',
+    'image' => 'http://example.com/images/promo.jpg',
+    'link' => 'http://example.com/promo',
+    'date' => '2023-05-18'
+  ),
+  array(
+    'name' => 'Lili',
+    'title' => 'Dicas para uma alimentação saudável',
+    'description' => 'Confira nossas dicas para ter uma alimentação saudável e equilibrada.',
+    'image' => 'http://example.com/images/healthy-food.jpg',
+    'link' => 'http://example.com/healthy-food-tips',
+    'date' => '2023-05-22'
+  ),
+  array(
+    'name' => 'Lili',
+    'title' => 'Novidades em nossa loja',
+    'description' => 'Acabamos de receber muitas novidades em nossa loja! Venha conferir.',
+    'image' => 'http://example.com/images/new-products.jpg',
+    'link' => 'http://example.com/new-products',
+    'date' => '2023-05-25'
+  ),
+  array(
+    'name' => 'Lili',
+    'title' => 'Dicas para cuidar dos cabelos',
+    'description' => 'Confira nossas dicas para manter seus cabelos saudáveis e bonitos.',
+    'image' => 'http://example.com/images/hair-care.jpg',
+    'link' => 'http://example.com/hair-care-tips',
+    'date' => '2023-05-29'
+  ),
+  array(
+    'name' => 'Lili',
+    'title' => 'Coleção de verão',
+    'description' => 'Chegou a nova coleção de verão! Muitas novidades para você aproveitar.',
+    'image' => 'http://example.com/images/summer-collection.jpg',
+    'link' => 'http://example.com/summer-collection',
+    'date' => '2023-06-01'
+  ),
+  array(
+    'name' => 'Lili',
+    'title' => 'Dicas para emagrecer',
+    'description' => 'Confira nossas dicas para emagrecer com saúde e manter o peso ideal.',
+    'image' => 'http://example.com/images/weight-loss.jpg',
+    'link' => 'http://example.com/weight-loss-tips',
+    'date' => '2023-06-05'
+  ),
+  array(
+    'name' => 'Lili',
+    'title' => 'Desconto especial para o Dia dos Namorados',
+    'description' => 'Compre o presente para seu amor com um desconto especial! Use o código AMOR10 e ganhe 10% de desconto.',
+    'image' => 'http://example.com/images/valentines-day.jpg',
+    'link' => 'http://example.com/valentines-day-sale',
+    'date' => '2023-06-08'
+  )
+);
+
+?>
